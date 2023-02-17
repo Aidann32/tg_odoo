0c632564e91338a62f64829bdc4cb9fcaa6e06d6
dev12 <46024391+Aidann32@users.noreply.github.com>
2023-02-17 15:32:17 +0500

    Initial

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Telegram module',
+    'description': 'This module sends all information about sended messages in Telegram Bot',
+    'version': '1.0',
+    'author': 'Aidan Ali',
+    'category': 'Tools'
+}
